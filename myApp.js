c62ace322e5756abbcefd3c914f84cb2b714babc
Glitch (timestamp-microservice--) <none>
2018-09-06 19:00:54 +0000

    comments added

--- myApp.js
+++ myApp.js
@@ -3,12 +3,17 @@ var app = express();
 
 app.get('/api/timestamp/:date_string?', 
         (req,res)=> {
-                      if (req.params.date_string.length===10){
+//                       checks if date_string is valid
+                      if (new Date(req.params.date_string) instanceof Date && 
+                          !isNaN(new Date(req.params.date_string).valueOf())){
                         res.json({
                           "unix": req.params.date_string.getTime(),
                           "utc" : req.params.date_string.toUTCString() 
                         })
-                      } else if (req.params.date_string===undefined){
+                      } 
+//                       checks if nothing is passed
+  else if (req.params.date_string===undefined &&
+                                req.params.date_string===null){
                         res.json({
                           "unix": new Date().getTime(),
                           "utc" : new Date().toUTCString() 
